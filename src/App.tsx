9bf500967037a392884bd13e058006c58a453c78
anand <anandpromax@gmail.com>
2024-05-19 22:27:40 +0530

    refactor: Update App and LineGraph components
    - Update App component to include an underline style for the title "ML job market"
    - Update LineGraph component to use formatted data and display a line chart of total jobs

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 
+import LineGraph from "./components/lineGraph";
 import EnhancedTable from "./components/mainTable";
 function App() {
   return (
     <div className="w-full flex p-10 gap-10">
       <div className="flex flex-col  w-1/2">
-        <h1 className="font-bold self-center text-xl">ML job market</h1>
+        <h1 className="font-bold self-center text-xl underline">ML job market</h1>
         {/* <MainTable /> */}
         <EnhancedTable/>
       </div>
-      <div>
-        <h1 className="font-bold self-center text-xl">Analysis</h1>
+      <div className="flex flex-col w-full"
+      >
+        <h1 className="font-bold self-center text-xl underline pb-4">Analysis</h1>
+        <LineGraph/>
       </div>
     </div>
   );
--- src/components/lineGraph.tsx
+++ src/components/lineGraph.tsx
@@ -1,6 +1,7 @@
  
 import { getJobDataByYear } from "../helper/formatedData";
-import React from "react";
+import { JSONData } from "./constants/constants";
+ 
 import { Line } from "react-chartjs-2";
 import {
   Chart,
@@ -11,14 +12,52 @@ import {
   LinearScale,
   Title,
 } from "chart.js";
-
-import { JSONData } from "./constants/constants";
-const jobData = getJobDataByYear(JSONData);
-console.log(jobData);
+Chart.register(
+  CategoryScale,
+  LineController,
+  LineElement,
+  PointElement,
+  LinearScale,
+  Title
+);
+ 
+ 
 const LineGraph = () => {
+const jobDataByYear = getJobDataByYear(JSONData);
+  const chartData = {
+    labels: Object.keys(jobDataByYear).map(Number),
+    datasets: [
+      {
+        label: "Total Jobs",
+        data: Object.values(jobDataByYear).map((data) => data.totalJobs),
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+      },
+    ],
+  };
+
+  const chartOptions = {
+    responsive: true,
+    plugins: {
+      title: {
+        display: true,
+         
+        text: "Total Jobs",
+      },
+    },
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+ 
   return (
-    <div>LineGraph</div>
-  )
-}
+    <div className="w-full  ">
+      <Line data={chartData} options={chartOptions} className="" />
+    </div>
+  );
+};
+
 
 export default LineGraph
